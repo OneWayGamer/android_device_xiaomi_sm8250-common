84f4de99d6ba3e9f00e9ef52bf9294166474293f
EmanuelCN <emanuelghub@gmail.com>
2022-07-24 12:35:16 +0200

    sm8250-common: Use prebuilt audiohalext
    *in device tree

--- kona.mk
+++ kona.mk
@@ -155,7 +155,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.1-impl \
     android.hardware.soundtrigger@2.3-impl \
-    vendor.qti.hardware.audiohalext@1.0.vendor \
     android.hardware.bluetooth.a2dp@1.0
 
 PRODUCT_PACKAGES += \
